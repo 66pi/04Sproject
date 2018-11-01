60c956dfff730ce75a0c0c16925970fcd1e22c42
chenjiefei <nlpx_dc@hotmail.com>
2018-11-01 18:51:32 +0800

    初始化用户管理和商品管理代码

---
+++ productmanagement/Backend/src/main/java/com/c1604s/productmanagement/ProductmanagementApplication.java
@@ -0,0 +1,12 @@
+package com.c1604s.productmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductmanagementApplication.class, args);
+    }
+}
