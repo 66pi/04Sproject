28e844bfb934771c76e58c895273afd4541dd755
chenjiefei <nlpx_dc@hotmail.com>
2018-11-03 11:53:38 +0800

    完成用户添加的基本逻辑

--- usermanagement/Backend/src/main/java/com/c1604s/usermanagement/UsermanagementApplication.java
+++ usermanagement/Backend/src/main/java/com/c1604s/usermanagement/UsermanagementApplication.java
@@ -1,9 +1,11 @@
 package com.c1604s.usermanagement;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.c1604s.usermanagement.dao")
 public class UsermanagementApplication {
 
     public static void main(String[] args) {
--- usermanagement/Backend/src/main/java/com/c1604s/usermanagement/controller/UserController.java
+++ usermanagement/Backend/src/main/java/com/c1604s/usermanagement/controller/UserController.java
@@ -1,16 +1,29 @@
 package com.c1604s.usermanagement.controller;
 
+import com.c1604s.usermanagement.dao.UserMapper;
 import com.c1604s.usermanagement.dto.UserRegisterDTO;
-import org.apache.catalina.User;
+import com.c1604s.usermanagement.po.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@EnableAutoConfiguration
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @PostMapping("/register")
     public Integer register(@RequestBody UserRegisterDTO userRegisterDTO){
-        return 1;
+
+        User user = new User();
+        user.setUsername(userRegisterDTO.getUsername());
+        user.setEncryptedPassword(userRegisterDTO.getPassword());
+        user.setIsDelete(false);
+        userMapper.insert(user);
+        return user.getUserid();
     }
 
     @GetMapping("/login")
