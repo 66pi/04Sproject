51434a52ffabe3c42422567f054e7c2d057f73bf
chenjiefei <nlpx_dc@hotmail.com>
2018-11-03 10:56:55 +0800

    添加用户注册接口

---
+++ usermanagement/Backend/src/main/java/com/c1604s/usermanagement/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.c1604s.usermanagement.controller;
+
+import com.c1604s.usermanagement.dto.UserRegisterDTO;
+import org.apache.catalina.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @PostMapping("/register")
+    public Integer register(@RequestBody UserRegisterDTO userRegisterDTO){
+        return 1;
+    }
+
+    @GetMapping("/login")
+    public void login(){       }
+
+    @GetMapping("/getUserInfo")
+    public void getUserInfo(){}
+
+    @PostMapping("/updateUserInfo")
+    public void updateUserInfo(){}
+
+    @PostMapping("/signoff")
+    public void signoff(){}
+}
